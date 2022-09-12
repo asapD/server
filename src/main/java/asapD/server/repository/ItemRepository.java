30b5650a3e90ef438f5e11687fc107272b39bf26
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-09-12 19:27:14 +0900

    feat: Repository annotation

--- src/main/java/asapD/server/repository/ItemRepository.java
+++ src/main/java/asapD/server/repository/ItemRepository.java
@@ -4,7 +4,7 @@ import asapD.server.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
--- src/main/java/asapD/server/repository/OrdersItemRepository.java
+++ src/main/java/asapD/server/repository/OrdersItemRepository.java
@@ -4,7 +4,7 @@ import asapD.server.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface OrdersItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
