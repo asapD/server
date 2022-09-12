93d99346deff46eda2d539964f8f1e29005841fd
dldmsql <yhy5913@naver.com>
2022-09-13 00:32:25 +0900

    chore: Integrate response style

---
+++ src/main/java/asapD/server/response/ApiResponse.java
@@ -0,0 +1,19 @@
+package asapD.server.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+
+  private final Integer status = HttpStatus.OK.value();
+
+  private String message;
+  private Object data;
+}
