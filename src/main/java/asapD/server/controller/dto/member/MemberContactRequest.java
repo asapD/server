86621fd2f5c0f61a9c581ffb508c635476546650
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-09-17 21:13:27 +0900

    fix: Add valication exception handler

--- src/main/java/asapD/server/controller/dto/member/MemberContactRequest.java
+++ src/main/java/asapD/server/controller/dto/member/MemberContactRequest.java
@@ -3,9 +3,12 @@ package asapD.server.controller.dto.member;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class MemberContactRequest {
 
     @ApiModelProperty(example = "전화번호")
+    @NotBlank(message = "값이 존재하지 않습니다")
     private String contact;
 }
--- src/main/java/asapD/server/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/asapD/server/controller/exception/GlobalExceptionHandler.java
@@ -1,15 +1,15 @@
 package asapD.server.controller.exception;
 
 import asapD.server.response.ExceptionResponse;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -17,12 +17,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler
+    @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ExceptionResponse> handleBadCredentialsExceptionHandler(BadCredentialsException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 ExceptionResponse.builder().status(HttpStatus.BAD_REQUEST.value()).message("로그인에 실패하였습니다")
                         .build()
         );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ExceptionResponse.builder().status(HttpStatus.BAD_REQUEST.value()).message("타입이 맞지 않습니다").build()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ExceptionResponse response = classifyError(e.getBindingResult());
+        return ResponseEntity.status(response.getStatus()).body(
+                ExceptionResponse.builder().status(response.getStatus()).message(response.getMessage())
+                        .build());
+    }
+
+    private ExceptionResponse classifyError(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ExceptionResponse.builder()
+                    .status(HttpStatus.BAD_REQUEST.value())
+                    .message(bindingResult.getFieldError().getDefaultMessage())
+                    .build();
+        }
+        return ExceptionResponse.builder().status(HttpStatus.INTERNAL_SERVER_ERROR.value()).message("서버에러 입니다")
+                .build();
     }
 
 
