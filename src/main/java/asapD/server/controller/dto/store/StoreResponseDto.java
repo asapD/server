f87b4178efa73fb323e762c193414a880801ff6e
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-09-19 03:17:47 +0900

    refactor: store paging

--- src/main/java/asapD/server/controller/dto/store/StoreResponseDto.java
+++ src/main/java/asapD/server/controller/dto/store/StoreResponseDto.java
@@ -1,12 +1,19 @@
 package asapD.server.controller.dto.store;
 
+import asapD.server.domain.Store;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
+
 @Data
 public class StoreResponseDto {
     private String name;
     private int owner;
     private String address;
+
+    public StoreResponseDto(Store store) {
+        name = store.getName();
+        owner = store.getOwner();
+        address = store.getAddress();
+    }
 }
--- src/main/java/asapD/server/service/StoreService.java
+++ src/main/java/asapD/server/service/StoreService.java
@@ -1,11 +1,13 @@
 package asapD.server.service;
 
 import asapD.server.controller.dto.store.StoreResponseDto;
+import asapD.server.domain.Store;
 import asapD.server.repository.StoreRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +17,13 @@ public class StoreService {
 
   private final StoreRepository storeRepository;
 
-  @Transactional
-  public List<StoreResponseDto> getStoreAll(Pageable pageable) {
+  public Page<StoreResponseDto> getStoreAll(Pageable pageable) {
 
-    List<StoreResponseDto> response = storeRepository.findAll().stream().map(store ->
-            StoreResponseDto.builder()
-                .name(store.getName())
-                .address(store.getAddress())
-                .owner(store.getOwner())
-                .build())
-        .collect(Collectors.toList());
+    Page<Store> result = storeRepository.findAll(pageable);
 
-    return response;
+    Page<StoreResponseDto> storeList = result.map(store -> new StoreResponseDto(store));
+
+    return storeList;
   }
 
 }
